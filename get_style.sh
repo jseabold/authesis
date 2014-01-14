47b5d35d72e9fc33c3f063d961df6a1e46f9f8ae
Skipper Seabold <jsseabold@gmail.com>
2014-01-14 10:54:38 -0500

    Add script to download what you need

---
+++ get_style.sh
@@ -0,0 +1,20 @@
+# https://subversion.american.edu/aisaac/authesis/authesis.htm
+
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/authesis.cls
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/aucas.clo
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/auecon.clo
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/aut10.clo
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/aut11.clo
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/aut12.clo
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/au-cms.bst
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/natbib.cfg
+
+# test proposal
+
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/auproposal.sty
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/test_auproposal.tex
+
+# test thesis and bib
+
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/autest.tex
+wget --no-check-certificate https://subversion.american.edu/aisaac/authesis/autest.bib
